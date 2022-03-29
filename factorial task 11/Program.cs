6d9545b902a1589bcf6a152f28a8241234b2435a
LUKE MCDONALD <103056462@student.swin.edu.au>
2022-03-30 10:47:25 +1100

    complete

---
+++ factorial task 11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace factorial_task_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number you want to find the factorial of:");
+            int num = int.Parse(Console.ReadLine());
+            int fact = factorial(num);
+            Console.WriteLine("The factorial is: " + fact);
+        }
+        static int factorial(int num)
+        {
+            int fact = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                
+                fact = fact * i;
+            }
+            return fact;
+        }
+    }
+}
---
+++ recursive fibonacci task 12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace recursive_fibonacci_task_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number you want to find the fibonacci number of: ");
+            int num = Fibonacci(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Fibonacci number is: " + num);
+        }
+
+        static int Fibonacci(int num)
+        {
+            List<int> FibonacciList = new List<int>();
+            FibonacciList.Add(0);
+            FibonacciList.Add(1);
+            int sum = 0;
+            for (int i = 2; i <= num; i++)
+            {
+                sum = FibonacciList[i - 2] + FibonacciList[i-1];
+                FibonacciList.Add(sum);
+            }
+            return sum;
+        }
+    }
+}
